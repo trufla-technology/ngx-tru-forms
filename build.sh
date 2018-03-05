c66de1d1aa064325a73760a2bd15241598e4a819
Boris Cosic <boriscosic@gmail.com>
2018-03-05 13:58:05 -0700

    Remove build

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/boris/Documents/projects/angular-json-form && \
-    npm run build && \
-    rsync -av . ../bullfrog-form/node_modules/angular-json-form/ --exclude node_modules --exclude src --exclude tests --exclude .git
