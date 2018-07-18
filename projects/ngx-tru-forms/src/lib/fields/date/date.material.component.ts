01898fdb5e84ae3813d0af859c33832a26a1cf9a
Boris Cosic <boriscosic@gmail.com>
2018-07-18 11:00:09 -0600

    Fix date formating

--- projects/ngx-tru-forms/src/lib/fields/date/date.material.component.ts
+++ projects/ngx-tru-forms/src/lib/fields/date/date.material.component.ts
@@ -1,19 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonComponent } from '../common/common.component';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+
+const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   template: `
     <mat-form-field>
       <input
         matInput
-        [matDatepicker]="myDatepicker"
-        [formControl]="control"
-        [placeholder]="title()"
+        [matDatepicker]='myDatepicker'
+        [formControl]='control'
+        [placeholder]='title()'
       >
-      <mat-datepicker-toggle matSuffix [for]="myDatepicker"></mat-datepicker-toggle>
+      <mat-datepicker-toggle matSuffix [for]='myDatepicker'></mat-datepicker-toggle>
       <mat-datepicker #myDatepicker></mat-datepicker>
-      <mat-error jf-error [control]="control"></mat-error>
+      <mat-error jf-error [control]='control'></mat-error>
     </mat-form-field>
-  `
+  `,
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE]
+    },
+    {
+      provide:
+      MAT_DATE_FORMATS,
+      useValue: MY_FORMATS
+    },
+  ],
 })
 export class DateMaterialComponent extends CommonComponent {}
