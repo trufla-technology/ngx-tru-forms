0c7713c5a167a224fba2f0a6efde34e2733db47a
Boris Cosic <boriscosic@gmail.com>
2018-07-31 18:55:40 -0600

    Remove material-moment-adapter

--- projects/ngx-tru-forms/src/lib/fields/date/date.material.component.ts
+++ projects/ngx-tru-forms/src/lib/fields/date/date.material.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonComponent } from '../common/common.component';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material';
-import {MomentDateAdapter} from '@angular/material-moment-adapter';
 
 const MY_FORMATS = {
   parse: {
@@ -28,18 +27,6 @@ const MY_FORMATS = {
       <mat-datepicker #myDatepicker></mat-datepicker>
       <mat-error jf-error [control]='control'></mat-error>
     </mat-form-field>
-  `,
-  providers: [
-    {
-      provide: DateAdapter,
-      useClass: MomentDateAdapter,
-      deps: [MAT_DATE_LOCALE]
-    },
-    {
-      provide:
-      MAT_DATE_FORMATS,
-      useValue: MY_FORMATS
-    },
-  ],
+  `
 })
 export class DateMaterialComponent extends CommonComponent {}
