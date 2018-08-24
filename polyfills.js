3a0974fa336f8a1136b1c98eb03cda6cfacf3ee7
Boris Cosic <boris.c@trufla.com>
2018-08-24 14:01:10 -0700

    Publishing 0.0.0

--- polyfills.js
+++ polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/boris/Documents/projects/ngx-tru-forms-app/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/boris/Documents/Trufla/projects/ngx-tru-forms-app/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
