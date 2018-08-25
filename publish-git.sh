113c83abd73738637c157661234dae11988434cb
Boris Cosic <boriscosic@gmail.com>
2018-08-24 20:00:10 -0700

    Build fix

--- publish-git.sh
+++ publish-git.sh
@@ -2,9 +2,10 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use 8
-mkdir -p build
-sed -i '' 's/forRoot(.*),/forRoot(JsonFormBootstrap4Module),/' src/app/json-schema-examples/json-schema-examples.module.ts
+cp src/index.html src/index.html.org
+mkdir -p build && ng build --base-href=/ngx-tru-forms/ --deploy-url=/ngx-tru-forms/ && cp -R dist/ngx-tru-forms-app/* build
+mkdir -p build/material && cp -R dist/ngx-tru-forms-app/* build/material
+sed -i '' 's/forRoot(.*),/forRoot(JsonFormMaterialModule),/' src/index.html
+sed -i '' 's/<!-- bootstrap4-cdn -->/<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">/' src/app/json-schema-examples/json-schema-examples.module.ts
 mkdir -p build/bootstrap4 && ng build --base-href=/ngx-tru-forms/bootstrap4/ --deploy-url=/ngx-tru-forms/bootstrap4/ && cp -R dist/ngx-tru-forms-app/* build/bootstrap4
-sed -i '' 's/forRoot(.*),/forRoot(JsonFormMaterialModule),/' src/app/json-schema-examples/json-schema-examples.module.ts
-ng build --base-href=/ngx-tru-forms/ --deploy-url=/ngx-tru-forms/ && cp -R dist/ngx-tru-forms-app/* build
-mkdir -p build/material && ng build --base-href=/ngx-tru-forms/material/ --deploy-url=/ngx-tru-forms/material/ && cp -R dist/ngx-tru-forms-app/* build/material
+mv src/index.html.org src/index.html
