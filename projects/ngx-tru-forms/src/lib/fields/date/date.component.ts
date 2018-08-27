fe91acfe31f5e342a1358c94d77c052b1a04a059
Boris Cosic <boriscosic@gmail.com>
2018-08-26 20:07:44 -0700

    Fix date

--- projects/ngx-tru-forms/src/lib/fields/date/date.component.ts
+++ projects/ngx-tru-forms/src/lib/fields/date/date.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonComponent } from '../common/common.component';
-import createNumberMask from 'text-mask-addons/dist/createNumberMask';
 
 @Component({
   template: `
@@ -13,16 +12,8 @@ import createNumberMask from 'text-mask-addons/dist/createNumberMask';
       [attr.type]="'date'"
       [formControl]="control"
       [placeholder]="placeholder()"
-      [textMask]="{ mask: numberMask}"
-      (input)=cleanMask($event.target.value)
     />
     <jf-error [control]="control"></jf-error>
   `
 })
-export class DateComponent extends CommonComponent {
-  numberMask = createNumberMask({ allowDecimal: false, prefix: '' });
-
-  cleanMask(value) {
-    this.control.setValue(value.replace(/\D/g, ''));
-  }
-}
+export class DateComponent extends CommonComponent {}
