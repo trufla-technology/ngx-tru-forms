ca0d827b952eec115dad6e105f08a93357339cd6
abdelhmed94 <abdelhameed.m@trufla.com>
2020-08-17 15:13:46 +0200

    fix wrong time in datepicker

--- projects/ngx-tru-forms/src/lib/fields/date/tru-ui-date/tru-ui-date.component.ts
+++ projects/ngx-tru-forms/src/lib/fields/date/tru-ui-date/tru-ui-date.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonComponent } from '../../common/common.component';
-
+import * as moment from 'moment';
+import { DateAdapter } from '@angular/material/core';
+import { MomentUtcDateAdapter } from '../../../services/moment-utc-adapter.service';
 @Component({
   selector: 'jf-tru-ui-date',
   templateUrl: './tru-ui-date.component.html',
-  styleUrls: ['../../../assets/tru-ui.css']
+  styleUrls: ['../../../assets/tru-ui.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentUtcDateAdapter
+    }
+  ]
 })
 export class TruUiDateComponent extends CommonComponent  {
+  oldValue;
+  isMobile() {
+    return window.innerWidth <= 800;
+  }
+
+  minDate() {
+    return isNaN(new Date(this.schema.minimum).getDate()) ? null : new Date(this.schema.minimum);
+  }
+
+  maxDate() {
+    return isNaN(new Date(this.schema.maximum).getDate()) ? null : new Date(this.schema.maximum);
+  }
+
+  onDateInput(e) {
+    const date = moment.utc(e).format('YYYY-MM-DD');
+    if (e && date !== this.oldValue) {
+    const newDate = new Date(`${moment.utc(e).format('YYYY-MM-DD')}T00:00:00.000Z`);
+    this.oldValue = moment.utc(e).format('YYYY-MM-DD');
+    this.control.setValue(newDate);
+    }
+  }
 }
