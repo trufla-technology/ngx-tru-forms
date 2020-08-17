3ba2df7cdc6ae82df0aab521fc951e2a512669a5
abdelhmed94 <abdelhameed.m@trufla.com>
2020-08-17 12:44:54 +0200

    fix ui bugs

--- docs/bootstrap4/polyfills-es2015.js
+++ docs/bootstrap4/polyfills-es2015.js
@@ -6238,8 +6238,8 @@ var __importDefault = (undefined && undefined.__importDefault) || function (mod)
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/george/projects/trufla/ngx-tru-forms/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /home/george/projects/trufla/ngx-tru-forms/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /home/abdelhameed/Desktop/ngx-tru-forms/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/abdelhameed/Desktop/ngx-tru-forms/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
