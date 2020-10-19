83056324c0d93516363460367d891c8698c4ed9e
abdelhmed94 <abdelhameed.m@trufla.com>
2020-10-19 12:57:20 +0200

    translate static values

--- projects/ngx-tru-forms/src/lib/fields/error/validation-feedback-translation.ts
+++ projects/ngx-tru-forms/src/lib/fields/error/validation-feedback-translation.ts
@@ -1,27 +1,27 @@
 export class ValidationFeedbackTranslation {
     validation = {
         en : {
-            required: "This field is required",
-            invalid: "Input value is invalid",
-            email_invalid: "Email is invalid",
-            minimum_invalid: "Input has to be a minimum of",
-            maximum_invalid: "Input can contain a maximum of",
-            characters: "characters",
-            greater_than: "Enter a value greater than or equal to",
-            less_than: "Enter a value less than or equal to ",
-            not_in_menu: " Select from the dropdown list"
-        }, 
+            required: 'This field is required',
+            invalid: 'Input value is invalid',
+            email_invalid: 'Email is invalid',
+            minimum_invalid: 'Input has to be a minimum of',
+            maximum_invalid: 'Input can contain a maximum of',
+            characters: 'characters',
+            greater_than: 'Enter a value greater than or equal to',
+            less_than: 'Enter a value less than or equal to ',
+            not_in_menu: ' Select from the dropdown list'
+        },
         fr: {
-            required: "Ce champ est requis.",
-            invalid: "Input value is invalid",
-            email_invalid: "Email is invalid",
-            minimum_invalid: "Input has to be a minimum of",
-            maximum_invalid: "Input can contain a maximum of",
-            characters: "characters",
-            greater_than: "Enter a value greater than or equal to",
-            less_than: "Enter a value less than or equal to ",
-            not_in_menu: " Select from the dropdown list"
+            required: 'Ce champ est requis.',
+            invalid: 'La valeur d entrée n est pas valide',
+            email_invalid: 'Le courriel est invalide',
+            minimum_invalid: 'Entrée doit être au minimum',
+            maximum_invalid: 'Entrée peut contenir un maximum de',
+            characters: 'charaPersonnagescters',
+            greater_than: 'Entrez une valeur supérieure ou égale à',
+            less_than: 'Entrez une valeur inférieure ou égale à',
+            not_in_menu: ' Sélectionnez dans la liste déroulante'
         }
-    }
+    };
 }
 
