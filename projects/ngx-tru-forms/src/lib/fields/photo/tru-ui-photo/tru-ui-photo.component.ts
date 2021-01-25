8178bf8de7aaeeb898a3cf6bff4396ca1f119344
abdelhmed94 <abdelhameed.m@trufla.com>
2021-01-25 06:07:02 +0200

    compress base64 images add validation for image upload size add validation for file type

--- projects/ngx-tru-forms/src/lib/fields/photo/tru-ui-photo/tru-ui-photo.component.ts
+++ projects/ngx-tru-forms/src/lib/fields/photo/tru-ui-photo/tru-ui-photo.component.ts
@@ -10,44 +10,28 @@ export class TruUiPhotoComponent extends CommonComponent {
   file: any ;
   selectFile = false;
   photoData: string;
+  busy = false;
+
   resetUpload() {
     this.file = {};
     this.selectFile = false;
     this.fileSize = null;
+    this.photoData = null;
+    this.busy = false;
     this.control.reset();
+  }
 
-  }
   async handleDrop(files) {
-    if (this.schema.maxSize && +this.schema.maxSize < +files[0].size/1024/1024) {
-      this.fileSize = null;
-      this.control.setErrors({maxSize: true});
-      this.control.markAsTouched();
-     } else { 
-      this.file = files[0];
-      this.fileSize = null;
-    this.selectFile = true;
-    const file = await this.toBase64(this.file);
-    this.photoData = file.toString();
-    this.control.setValue(this.photoData);
-     }
+    console.log('handleDrop')
+   this.processFile(files);
   }
+
   shortenSize(data) {
     return data.toString().substring(0, 5);
   }
 
   async dragAndDrop(files) {
-    if (this.schema.maxSize && +this.schema.maxSize < +files.target.files[0].size/1024/1024) {
-      this.fileSize = null;
-     this.control.setErrors({maxSize: true});
-     this.control.markAsTouched();
-    } else { 
-      this.file = files.target.files[0];
-      this.fileSize = null;
-    this.selectFile = true;
-    const file = await this.toBase64(this.file);
-    this.photoData = file.toString();
-    this.control.setValue(this.photoData);
-  }
+    this.processFile(files.target.files)
   }
 
   toBase64(file): Promise<any> {
@@ -57,5 +41,50 @@ export class TruUiPhotoComponent extends CommonComponent {
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
   });
+  }
+
+ async processFile(files) {
+    const name = files[0].name;
+    const file = await this.toBase64(files[0]);
+    const lastDot = name.lastIndexOf('.');
+    const ext = name.substring(lastDot + 1);
+    if ((this.schema.maxSize && +this.schema.maxSize < +files[0].size/1024/1024) ||
+        (this.schema.imageFormat && [...this.schema.imageFormat].indexOf(ext.toLowerCase()) === -1 )) {
+      this.fileSize = null;
+      this.busy = false;
+      (this.schema.maxSize && +this.schema.maxSize < +files[0].size/1024/1024) ? this.control.setErrors({maxSize: true}) : this.control.setErrors({invalidFormat: true});
+      this.control.markAsTouched();
+      return
+     } else { 
+       this.busy = true;
+      this.file = files[0];
+      this.fileSize = null;
+      this.selectFile = true;
+      if ( ['png', 'jpeg', 'jpg', 'jpeg', 'gif'].indexOf(ext.toLowerCase()) !== -1 ) {
+        this.compressFile( file.toString()).then((res) => {
+        this.getImageFromUrl(res);
+        this.photoData = res;
+        this.busy = false;
+        this.control.setValue(this.photoData);
+    }, (err) => {
+      this.fileSize = null;
+      this.busy = false;
+      this.control.setErrors({invalid: true});
+      this.control.markAsTouched();
+      this.busy = false;
+    }).catch((err) => {
+      this.fileSize = null;
+      this.busy = false;
+      this.control.setErrors({invalid: true});
+      this.control.markAsTouched();
+      this.busy = false;
+    });
+    } else {
+      this.photoData = file.toString();
+      this.getImageFromUrl(file.toString());
+      this.busy = false;
+      this.control.setValue(this.photoData);
+    }
+   }
   }
 }
