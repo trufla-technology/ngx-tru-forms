0d1405f7696c8f53f5085a508a9f6645cfd974f5
abdelhameed-maged <abdelhameed.m@trufla.com>
2021-11-07 07:23:33 +0200

    upgrade to v12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,6 +55,6 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
