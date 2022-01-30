f81791d22c9d961020bbafa86a7f3f08a8c4bb1b
abdelhameed-maged <abdelhameed.m@trufla.com>
2022-01-30 15:33:07 +0200

    fix upload file translation

--- docs/src_app_modules_material-design_material-design_module_ts-es2015.js
+++ docs/src_app_modules_material-design_material-design_module_ts-es2015.js
@@ -12,11 +12,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "MaterialDesignComponent": function() { return /* binding */ MaterialDesignComponent; }
 /* harmony export */ });
 /* harmony import */ var _json_schema_examples_input_colour_input_colour_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../json-schema-examples/input-colour/input-colour.component */ 5505);
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/core */ 2316);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ 2316);
 /* harmony import */ var _services_schema_listener_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../services/schema-listener.service */ 66338);
 /* harmony import */ var _services_data_listener_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../services/data-listener.service */ 19648);
-/* harmony import */ var _trufla_ngx_tru_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @trufla/ngx-tru-forms */ 80988);
-
 
 
 
@@ -37,14 +35,14 @@ class MaterialDesignComponent {
         this.btnDisabled = true;
     }
 }
-MaterialDesignComponent.ɵfac = function MaterialDesignComponent_Factory(t) { return new (t || MaterialDesignComponent)(_angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdirectiveInject"](_services_schema_listener_service__WEBPACK_IMPORTED_MODULE_1__.SchemaListenerService), _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdirectiveInject"](_services_data_listener_service__WEBPACK_IMPORTED_MODULE_2__.DataListenerService)); };
-MaterialDesignComponent.ɵcmp = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵdefineComponent"]({ type: MaterialDesignComponent, selectors: [["app-material-design"]], decls: 1, vars: 3, consts: [[3, "schema", "fields", "submit", "handleSubmit"]], template: function MaterialDesignComponent_Template(rf, ctx) { if (rf & 1) {
-        _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵelementStart"](0, "jf-form", 0);
-        _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵlistener"]("handleSubmit", function MaterialDesignComponent_Template_jf_form_handleSubmit_0_listener($event) { return ctx.handleSubmit($event); });
-        _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵelementEnd"]();
+MaterialDesignComponent.ɵfac = function MaterialDesignComponent_Factory(t) { return new (t || MaterialDesignComponent)(_angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdirectiveInject"](_services_schema_listener_service__WEBPACK_IMPORTED_MODULE_1__.SchemaListenerService), _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdirectiveInject"](_services_data_listener_service__WEBPACK_IMPORTED_MODULE_2__.DataListenerService)); };
+MaterialDesignComponent.ɵcmp = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineComponent"]({ type: MaterialDesignComponent, selectors: [["app-material-design"]], decls: 1, vars: 3, consts: [[3, "schema", "fields", "submit", "handleSubmit"]], template: function MaterialDesignComponent_Template(rf, ctx) { if (rf & 1) {
+        _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵelementStart"](0, "jf-form", 0);
+        _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵlistener"]("handleSubmit", function MaterialDesignComponent_Template_jf_form_handleSubmit_0_listener($event) { return ctx.handleSubmit($event); });
+        _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵelementEnd"]();
     } if (rf & 2) {
-        _angular_core__WEBPACK_IMPORTED_MODULE_4__["ɵɵproperty"]("schema", ctx.schema)("fields", ctx.fields)("submit", "submit");
-    } }, directives: [_trufla_ngx_tru_forms__WEBPACK_IMPORTED_MODULE_3__.JsonFormComponent], styles: ["\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYXRlcmlhbC1kZXNpZ24uY29tcG9uZW50LmNzcyJ9 */"] });
+        _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵproperty"]("schema", ctx.schema)("fields", ctx.fields)("submit", "submit");
+    } }, styles: ["\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYXRlcmlhbC1kZXNpZ24uY29tcG9uZW50LmNzcyJ9 */"] });
 
 
 /***/ }),
@@ -59,13 +57,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "MaterialDesignModule": function() { return /* binding */ MaterialDesignModule; }
 /* harmony export */ });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ 2316);
 /* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common */ 54364);
 /* harmony import */ var _material_design_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./material-design.component */ 41139);
-/* harmony import */ var _trufla_ngx_tru_forms__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @trufla/ngx-tru-forms */ 80988);
+/* harmony import */ var _trufla_ngx_tru_forms__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @trufla/ngx-tru-forms */ 26739);
 /* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/router */ 71258);
 /* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/forms */ 1707);
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ 2316);
-
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 
 
 
@@ -78,11 +81,12 @@ const routes = [
         component: _material_design_component__WEBPACK_IMPORTED_MODULE_0__.MaterialDesignComponent
     }
 ];
-class MaterialDesignModule {
-}
-MaterialDesignModule.ɵfac = function MaterialDesignModule_Factory(t) { return new (t || MaterialDesignModule)(); };
-MaterialDesignModule.ɵmod = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineNgModule"]({ type: MaterialDesignModule });
-MaterialDesignModule.ɵinj = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineInjector"]({ imports: [[
+let MaterialDesignModule = class MaterialDesignModule {
+};
+MaterialDesignModule = __decorate([
+    (0,_angular_core__WEBPACK_IMPORTED_MODULE_2__.NgModule)({
+        declarations: [_material_design_component__WEBPACK_IMPORTED_MODULE_0__.MaterialDesignComponent],
+        imports: [
             _angular_common__WEBPACK_IMPORTED_MODULE_3__.CommonModule,
             _angular_router__WEBPACK_IMPORTED_MODULE_4__.RouterModule.forChild(routes),
             _angular_forms__WEBPACK_IMPORTED_MODULE_5__.ReactiveFormsModule,
@@ -97,9 +101,10 @@ MaterialDesignModule.ɵinj = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODUL
                     }
                 ]
             }
-        ]] });
-(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵsetNgModuleScope"](MaterialDesignModule, { declarations: [_material_design_component__WEBPACK_IMPORTED_MODULE_0__.MaterialDesignComponent], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_3__.CommonModule, _angular_router__WEBPACK_IMPORTED_MODULE_4__.RouterModule, _angular_forms__WEBPACK_IMPORTED_MODULE_5__.ReactiveFormsModule,
-        _trufla_ngx_tru_forms__WEBPACK_IMPORTED_MODULE_1__.JsonFormMaterialModule, _trufla_ngx_tru_forms__WEBPACK_IMPORTED_MODULE_1__.JsonFormModule] }); })();
+        ],
+    })
+], MaterialDesignModule);
+
 
 
 /***/ })
