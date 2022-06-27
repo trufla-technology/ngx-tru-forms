cebde47fdb115e78329c629ac188a3628c29c3de
abdelhameed-maged <abdelhameed.m@trufla.com>
2022-06-27 20:43:57 +0200

    create new dropdown with cdk overlay

--- projects/ngx-tru-forms/src/lib/fields/select/tru-ui-select/tru-ui-select.component.ts
+++ projects/ngx-tru-forms/src/lib/fields/select/tru-ui-select/tru-ui-select.component.ts
@@ -1,18 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener, AfterViewInit } from '@angular/core';
 import { CommonComponent } from '../../common/common.component';
+import { CdkOverlayOrigin } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'jf-tru-ui-select',
   templateUrl: './tru-ui-select.component.html',
   styleUrls: ['../../../assets/tru-ui.css']
 })
-export class TruUiSelectComponent extends CommonComponent {
-  returnFocus(e) {
+export class TruUiSelectComponent extends CommonComponent implements AfterViewInit  {
+  oldValue;
+  show = false;
+  isOpen = false;
+
+  @ViewChild(CdkOverlayOrigin) calendarOverlay;
+  returnFocus(e?) {
     setTimeout(() => {
      const select = document.getElementById(this.schema.key);
      if (select) {
        select.focus();
      }
     }, 100);
+  }
+
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+     if (event.code === 'Escape') {
+      this.isOpen = false;
+    }
+  }
+
+  ngAfterViewInit() {
+    this._adapter.setLocale(this.language || 'en');
+    if (this.control.data ) {
+  
+    this.cd.detectChanges();
+    }
+  }
+
+  isMobile() {
+    return window.innerWidth <= 800;
+  }
+
+ setSelectVaule(en) {
+
+ }
+  close(ev) {
+    this.isOpen = false;
+    this.returnFocus();
   }
 }
